79dbd6471b6c4f9d4ba4a7de03f47c06efa6b090
ShoaibHassan11 <soaebhasan12@users.noreply.github.com>
2025-09-30 10:38:22 +0530

    add app_name

--- organBridge/matches/urls.py
+++ organBridge/matches/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'matches'
+
 urlpatterns = [
     path('find/', views.find_matches, name='find_matches'),
     path('my-matches/', views.my_matches, name='my_matches'),
